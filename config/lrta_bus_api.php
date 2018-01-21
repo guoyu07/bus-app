9726bea102f17918264838091ca8de707eb6780b
Inon Baguio <inon.baguio@yahoo.com>
2018-01-21 05:39:07 +0000

    lrta bus api config

---
+++ config/lrta_bus_api.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | MyTransportSG API Link
+    |--------------------------------------------------------------------------
+    |
+    | API Documentation:
+    |      -  https://www.mytransport.sg/content/dam/mytransport/DataMall_StaticData/LTA_DataMall_API_User_Guide.pdf
+    |
+    */
+
+    /**
+     * API KEY
+     */
+    'key' => env('LRTA_API_KEY'),
+
+    /**
+     * API URL
+     */
+    'url' => 'http://datamall2.mytransport.sg/ltaodataservice/'
+];
