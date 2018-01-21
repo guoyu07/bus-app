ccdc4982731c33940db96b1a37e4df93ebc9e58c
Inon Baguio <inon.baguio@yahoo.com>
2018-01-21 13:58:49 +0000

    Fixed App\User references

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('bus-stops', 'BusStopsController');
