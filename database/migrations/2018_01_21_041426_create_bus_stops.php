66e229b37d196bee85a581ac8e7245f285c40bd7
Inon Baguio <inon.baguio@yahoo.com>
2018-01-21 12:48:31 +0800

    Migrations for Bus and User

--- database/migrations/2018_01_21_041426_create_bus_stops.php
+++ database/migrations/2018_01_21_041426_create_bus_stops.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateBusStops extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('bus_stops', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('code');
+            $table->string('road_name');
+            $table->string('description');
+            $table->double('latitude');
+            $table->double('longitude');
             $table->softDeletes();
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('bus_stops');
     }
 }
